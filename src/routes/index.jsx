b786888517569db8f1555d71a9a0a948865a5899
saidazam073073 <saidazam073sodiqboyev@gmail.com>
2025-08-07 17:27:44 +0500

    home

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,13 @@
 import { createBrowserRouter } from "react-router";
 import RootLayout from "../layout/RootLayout";
+import Home from "../components/Home";
 
 export const routes = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
     children: [
-      { index: true, element: <div>home</div> },
+      { index: true, element: <Home /> },
       { path: "shop", element: <div>Shop</div> },
       { path: "categories", element: <div>Categories</div> },
       { path: "contacts", element: <div>Contacts</div> },
